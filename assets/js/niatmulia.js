bc986068de352a020607d5de7bdbffa1030a870e
PensilCo <pensilco@gmail.com>
2015-09-09 12:06:09 +0700

    init project

---
+++ assets/js/niatmulia.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var niatmulia = (function ($) {
+
+    var indexPostClass = '.niatmulia-index-post',
+        mobileMenuButton = '.niatmulia-mobile-menu a',
+        mobileMenuCloseButton = '.niatmulia-mobile-close-btn',
+        mainMenu = '.niatmulia-menu',
+        bgCheckClass = '.bg-check',
+        postBgImages = '.bg-img img',
+        postCoverImg = '.niatmulia-post-header .bg-img',
+
+    // post animations on homepage
+    indexPostAnimate = function () {
+        if ($(indexPostClass).length) {
+            $(indexPostClass).each(function () {
+            var postPos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop(),
+                windowHeight = $(window).height();
+                if (postPos < topOfWindow + (windowHeight/ 1.4)) {
+                    $(this).addClass('fadeInDown');
+                }
+            });
+        }
+    },
+
+    mobileMenu = function () {
+        if($(mainMenu).length) {
+            $(mobileMenuButton).on('click', function(e){
+                e.preventDefault();
+                $(mainMenu).addClass('opened');
+            });
+            $(mobileMenuCloseButton).on('click', function(e){
+                e.preventDefault();
+                $(mainMenu).removeClass('opened');
+            });
+        }
+    },
+
+    headerTitlesBackgroundCheck = function () {
+        if ($(bgCheckClass).length && $(postBgImages).length) {
+            BackgroundCheck.init({
+                targets: bgCheckClass,
+                images: postBgImages
+            });
+        }
+    },
+
+    postHeaderCoverImg = function () {
+        var coverImage = $('[alt=cover-image]');
+        if (coverImage.length) {
+            $(postCoverImg).append('<img src="' + coverImage.attr('src') + '">');
+            coverImage.remove();
+        }
+    },
+
+    // niatmulia javascripts initialization
+    init = function () {
+        indexPostAnimate();
+        $(window).on('scroll', function() {
+            indexPostAnimate();
+        });
+        postHeaderCoverImg();
+        mobileMenu();
+        headerTitlesBackgroundCheck();
+        $('p:has(> img)').addClass('with-image');
+    };
+
+    return {
+        init: init
+    };
+
+})(jQuery);
+
+(function () {
+    niatmulia.init();
+})();
